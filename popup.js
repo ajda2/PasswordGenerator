cc18af4bc74982cb29bd00878f685ea9f6d80cd8
Michal Tichý <michal.tichy@mysurface.cz>
2014-07-19 14:58:23 +0200

    Settings block added

--- popup.js
+++ popup.js
@@ -7,9 +7,12 @@ var PasswordGenerator = PasswordGenerator || {};
  * @type {{passwordInputId: string, lengthInputId: string, submitButtonId: string}}
  */
 PasswordGenerator.config = {
-    passwordInputId: 'password',
-    lengthInputId:   'length',
-    submitButtonId:  'submit'
+    passwordInputId:           'password',
+    lengthInputId:             'length',
+    submitButtonId:            'submit',
+    settingsButtonId:          'settingsButton',
+    settingsHolderId:          'settingsHolder',
+    settingsButtonActiveClass: 'active'
 };
 
 /**
@@ -70,6 +73,57 @@ PasswordGenerator.passwordInputFocus = function () {
 };
 
 /**
+ * Settings button click listener
+ */
+PasswordGenerator.settingsButtonListener = function () {
+    var settingsHolder = document.getElementById(PasswordGenerator.config.settingsHolderId);
+
+    if (PasswordGenerator.hasClass(this, PasswordGenerator.config.settingsButtonActiveClass)) {
+        PasswordGenerator.removeClass(settingsHolder, PasswordGenerator.config.settingsButtonActiveClass);
+        PasswordGenerator.removeClass(this, PasswordGenerator.config.settingsButtonActiveClass);
+    }
+    else {
+        PasswordGenerator.addClass(settingsHolder, PasswordGenerator.config.settingsButtonActiveClass);
+        PasswordGenerator.addClass(this, PasswordGenerator.config.settingsButtonActiveClass);
+    }
+};
+
+/**
+ * Pure JavaScript Element has class check
+ * @param el
+ * @param className
+ * @return {Boolean}
+ */
+PasswordGenerator.hasClass = function (el, className) {
+    var className = " " + className + " ";
+    var elemClassName = el.getAttribute("class");
+    if ((" " + elemClassName + " ").replace(/[\n\t]/g, " ").indexOf(className) > -1) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * Add class to element
+ * @param el
+ * @param className
+ */
+PasswordGenerator.addClass = function (el, className) {
+    el.className = el.className.replace(className, ""); // first remove the class name if that already exists
+    el.className = el.className + className;
+};
+
+/**
+ * Remove class to element
+ * @param el
+ * @param className
+ */
+PasswordGenerator.removeClass = function (el, className) {
+    el.className = el.className.replace(className, ""); // first remove the class name if that already exists
+};
+
+/**
  * DOM loaded
  */
 document.addEventListener('DOMContentLoaded', function () {
@@ -80,4 +134,6 @@ document.addEventListener('DOMContentLoaded', function () {
     passwordInput.value = PasswordGenerator.generatePassword(lengthInput.value, true, true, true, true);
     submitButton.addEventListener('click', PasswordGenerator.submitButtonClickListener);
     passwordInput.addEventListener('click', PasswordGenerator.passwordInputFocus);
+
+    document.getElementById(PasswordGenerator.config.settingsButtonId).addEventListener('click', PasswordGenerator.settingsButtonListener);
 });
